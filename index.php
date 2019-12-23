2bc5b750207d0d5046b832b6f797d92cb6d2e035
Kevin Ivers <kivers08@gmail.com>
2019-12-22 20:25:41 -0500

    First commit, everything but the spacing and buttons are styled.

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Google</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div>
+  <ul>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Store</a></li>
+    <li style="float:right"><a href="#">Images</a></li>
+    <li style="float:right"><a href="#">Gmail</a></li>
+  </ul>
+</div>
+<div> <br>
+  <br>
+  <center>
+    <img src="images/googlelogo_color_272x92dp.png" width="272" height="92" alt="Google Logo"/>
+  </center>
+  <br>
+</div>
+<div>
+  <form>
+    <center>
+    <input type="search" class="searchbox">
+    <br>
+    <br>
+    <input type="submit" class="main-buttons">
+    <input type="button" value="Im feeling lucky" class="main-buttons">
+  </form>
+	  </center>
+</div>
+<div id="footer">
+  <ul>
+    <li><a href="#">Advertising</a></li>
+    <li><a href="#">Business</a></li>
+    <li><a href="#">How search works</a></li>
+    <li style="float:right"><a href="#">Settings</a></li>
+    <li style="float:right"><a href="#">Terms</a></li>
+    <li style="float:right"><a href="#">Privacy</a></li>
+  </ul>
+</div>
+</body>
+</html>
